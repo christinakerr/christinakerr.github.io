875bf3813f05293272bf259f94d755f525990c00
Christi Kerr <christinakerr394@gmail.com>
2020-11-22 10:23:27 -0600

    Built router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar"
+import About from "./components/About"
+import Portfolio from "./components/Portfolio"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Route exact path="/" component={About}/>
+      <Route exact path="/portfolio" component={Portfolio}/>
+      <Route exact path="/contact" component={Contact}/>
+      <Footer />
+    </Router>
   );
 }
 
