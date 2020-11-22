346337296869f3689933e7c4dfe5b4d055bde6a3
Christi Kerr <christinakerr394@gmail.com>
2020-11-22 11:01:20 -0600

    Built footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function Footer() {
     return(
-        <div>Footer</div>
+        <footer class="fixed-bottom bg-dark">
+            <p class="foot">Copyright 2020 Christina Kerr || <span class="socials"><a href="https://www.linkedin.com/in/christinakerr/" rel="noreferrer" target="_blank"><i class="fab fa-linkedin-in"></i></a></span>
+                <a href="https://github.com/christinakerr" rel="noreferrer" target="_blank"><i class="fab fa-github"></i></a></p>
+        </footer>
     )
 }
 
