64e041f044cde0faad024978410e02f208707e28
Christi Kerr <christinakerr394@gmail.com>
2020-11-23 16:11:18 -0600

    Added custom favicon

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import image from "../christinaKerr2.JPG"
 
 function About() {
     return(
@@ -10,9 +11,11 @@ function About() {
             </header>
             <div className="row">
                 <div className="col-md-12">
-                    <img src="images/christinaKerr.JPG" alt="Christina Kerr" width="200" height="200" className="float-left" />
-                    <p>Full-stack web developer with a content writing & video production background. Skilled in HTML, CSS, and JavaScript. Strong written and oral communication skills with a passion for creative problem-solving. Known among colleagues for attention to detail, organization, and positive attitude. Certificate in full-stack web development from the University of Texas boot camp. Bachelor of Science in Mass Communication from Texas State University. Certification in technical writing with the Society for Technical Communications. I’m excited to find a position that allows me to use both my creativity and critical thinking skills.</p>
-                    <p>When I'm not developing websites, I spend my time knitting, playing tabletop games with my friends (virtually) and hanging out with my spaniel mix, Calvin.</p>
+                    <img src={image} alt="Christina Kerr" width="200" height="200" className="float-left" />
+                    <h2 className="name">Christina Kerr</h2>
+                    <hr />
+                    <p className="homepage">Full-stack web developer with a content writing & video production background. Skilled in HTML, CSS, and JavaScript. Strong written and oral communication skills with a passion for creative problem-solving. Known among colleagues for attention to detail, organization, and positive attitude. Certificate in full-stack web development from the University of Texas boot camp. Bachelor of Science in Mass Communication from Texas State University. Certification in technical writing with the Society for Technical Communications. I’m excited to find a position that allows me to use both my creativity and critical thinking skills.</p>
+                    <p className="homepage">When I'm not developing websites, I spend my time knitting, playing tabletop games with my friends (virtually) and hanging out with my spaniel mix, Calvin.</p>
                 </div>
             </div>
         </main>
